f863a824c38cd18f6b8a9a75b7bdf05aa448cebc
qixiaobro <zxd982092690@gmail.com>
2020-12-14 17:55:39 +0800

    feat:合并路由文件

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,21 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import Home from "../views/Home.vue";
 
+/** 自动加载其他路由模块 */
+const files = require.context(".", true, /\.ts$/);
+const modules: Array<RouteRecordRaw> = [];
+files.keys().forEach(key => {
+  if (key === "./index.ts") return;
+  modules.push(files(key).default);
+});
+
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "Home",
     component: Home
   },
+  ...modules,
   {
     path: "/about",
     name: "About",
